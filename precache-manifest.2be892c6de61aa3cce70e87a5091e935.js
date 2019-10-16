154e78021ee3b0486658c2c737e2b9331de782fc
Venu Gopal <venugopalkr296@gmail.com>
2019-10-17 01:17:32 +0530

    Updates

---
+++ precache-manifest.2be892c6de61aa3cce70e87a5091e935.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55c8a83fac58e8400c0b9a9f3b580c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "713e4d7dad350afa39e2",
+    "url": "/static/css/main.99238fba.chunk.css"
+  },
+  {
+    "revision": "47f98e917b905446c750",
+    "url": "/static/js/2.fd2fe45c.chunk.js"
+  },
+  {
+    "revision": "713e4d7dad350afa39e2",
+    "url": "/static/js/main.15e8b2ec.chunk.js"
+  },
+  {
+    "revision": "f89f26abec2751bae34e",
+    "url": "/static/js/runtime~main.672b2fcf.js"
+  },
+  {
+    "revision": "0e6bd41159754339a9929dd1918919f9",
+    "url": "/static/media/WoodTheme.0e6bd411.jpg"
+  },
+  {
+    "revision": "ae959e77080ab5ae6bf43f59d1f6287b",
+    "url": "/static/media/grant-ritchie-x1w_Q78xNEY-unsplash.ae959e77.jpg"
+  },
+  {
+    "revision": "884ce736a7e391daee7f52728362baf8",
+    "url": "/static/media/—Pngtree—panda xiong bao baby hand-painted_3860457.884ce736.png"
+  }
+]);
